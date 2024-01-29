fa526cc0ecd76bc0a9cfc01b1b9dbfde6baf9c01
Mohamed Abdelhalim <95293830+1abdelhalim@users.noreply.github.com>
2024-01-29 18:48:54 +0200

    Time: 34 ms (74.80%) | Memory: 16.6 MB (68.49%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.py
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.py
@@ -1,24 +1,28 @@
-from collections import deque
-
 class MyQueue:
 
     def __init__(self):
-        self.data = deque()
+        self.stack1 = []  # main stack for enqueue
+        self.stack2 = []  # auxiliary stack for dequeue
 
     def push(self, x: int) -> None:
-        self.data.append(x)
+        self.stack1.append(x)
 
     def pop(self) -> int:
-        if not self.empty():
-            return self.data.popleft()
+        if not self.stack2:
+            # Transfer elements from stack1 to stack2
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2.pop() if self.stack2 else None
 
     def peek(self) -> int:
-        if not self.empty():
-            return self.data[0]
+        if not self.stack2:
+            # Transfer elements from stack1 to stack2
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2[-1] if self.stack2 else None
 
     def empty(self) -> bool:
-        return len(self.data) == 0
-
+        return not self.stack1 and not self.stack2
 
 # Your MyQueue object will be instantiated and called as such:
 # obj = MyQueue()
