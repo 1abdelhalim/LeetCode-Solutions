274074ffde75a407647fb7d53da35e9222d06b36
Mohamed Abdelhalim <95293830+1abdelhalim@users.noreply.github.com>
2024-02-27 06:02:53 +0200

    Time: 33 ms (83.42%) | Memory: 16.5 MB (90.84%) - LeetSync

---
+++ 71-simplify-path/simplify-path.py
@@ -0,0 +1,14 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        n = len(path)
+        stack = []
+        sett = {".", "", ".."}
+
+        path = path.split("/")
+        for ch in path:
+            if stack and ch == "..":
+                stack.pop()
+            elif ch not in sett:
+                stack.append(ch)
+
+        return "/" + "/".join(stack)
