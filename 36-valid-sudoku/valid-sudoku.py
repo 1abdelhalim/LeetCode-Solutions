608af53eefa7ae8d792ade08717a1e9519e15c72
Mohamed Abdelhalim <95293830+1abdelhalim@users.noreply.github.com>
2024-03-01 20:56:02 +0200

    Time: 97 ms (34.42%) | Memory: 16.6 MB (76.07%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,30 +1,8 @@
-class Solution(object):
-    def isValidSudoku(self, board):
-        # Check rows
-        for row in board:
-            if not self.is_valid_row(row):
-                return False
-        
-        # Check columns
-        for col in range(9):
-            column = [board[i][col] for i in range(9)]
-            if not self.is_valid_row(column):
-                return False
-        
-        # Check sub-boxes
-        for i in range(0, 9, 3):
-            for j in range(0, 9, 3):
-                sub_box = [board[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]
-                if not self.is_valid_row(sub_box):
-                    return False
-        
-        return True
-
-    def is_valid_row(self, row):
-        seen = set()
-        for cell in row:
-            if cell != '.':
-                if cell in seen:
-                    return False
-                seen.add(cell)
-        return True
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        ans = []
+        for i, row in enumerate(board):
+            for j, val in enumerate(row):
+                if val != '.':
+                    ans += [(i, val), (val, j), (i // 3, j // 3, val)]
+        return len(ans) == len(set(ans))
