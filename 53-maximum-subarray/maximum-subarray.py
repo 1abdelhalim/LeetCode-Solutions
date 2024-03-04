e8f59964d67cbac79c3b15c1ffc3cb19ab0bb117
Mohamed Abdelhalim <95293830+1abdelhalim@users.noreply.github.com>
2024-03-04 14:05:57 +0200

    Time: 549 ms (40.54%) | Memory: 31 MB (73.35%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -4,8 +4,7 @@ class Solution:
         curr_sum = 0 
 
         for num in nums:
-            if curr_sum < 0:
-                curr_sum = 0
+            curr_sum = max(curr_sum, 0)
             curr_sum += num
             max_sum = max(max_sum, curr_sum)
 
