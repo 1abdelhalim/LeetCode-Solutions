d767d7d4d75a92d76153f27bc0ec8cde4e43a79d
Mohamed Abdelhalim <95293830+1abdelhalim@users.noreply.github.com>
2024-03-11 11:07:23 +0200

    Time: 447 ms (54.51%) | Memory: 16.6 MB (54.98%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -1,44 +1,30 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-	
-        def kSum(nums: List[int], target: int, k: int) -> List[List[int]]:
-            res = []
-            
-            # If we have run out of numbers to add, return res.
-            if not nums:
-                return res
-            
-            # There are k remaining values to add to the sum. The 
-            # average of these values is at least target // k.
-            average_value = target // k
-            
-            # We cannot obtain a sum of target if the smallest value
-            # in nums is greater than target // k or if the largest 
-            # value in nums is smaller than target // k.
-            if average_value < nums[0] or nums[-1] < average_value:
-                return res
-            
-            if k == 2:
-                return twoSum(nums, target)
-    
-            for i in range(len(nums)):
-                if i == 0 or nums[i - 1] != nums[i]:
-                    for subset in kSum(nums[i + 1:], target - nums[i], k - 1):
-                        res.append([nums[i]] + subset)
-    
-            return res
-
-        def twoSum(nums: List[int], target: int) -> List[List[int]]:
-            res = []
-            s = set()
-    
-            for i in range(len(nums)):
-                if len(res) == 0 or res[-1][1] != nums[i]:
-                    if target - nums[i] in s:
-                        res.append([target - nums[i], nums[i]])
-                s.add(nums[i])
-    
-            return res
-
         nums.sort()
-        return kSum(nums, target, 4)
+        quadruplets = []
+        n = len(nums)
+
+        for i in range(n - 3):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i + 1, n - 2):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                left = j + 1
+                right = n - 1
+                while left < right:
+                    total = nums[i] + nums[j] + nums[left] + nums[right]
+                    if total == target:
+                        quadruplets.append([nums[i], nums[j], nums[left], nums[right]])
+                        while left < right and nums[left] == nums[left + 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right - 1]:
+                            right -= 1
+                        left += 1
+                        right -= 1
+                    elif total < target:
+                        left += 1
+                    else:
+                        right -= 1
+
+        return quadruplets
