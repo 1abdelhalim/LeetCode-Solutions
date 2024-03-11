32d8255c4dde809d1167fe94c50b225a98ac4518
Mohamed Abdelhalim <95293830+1abdelhalim@users.noreply.github.com>
2024-03-11 11:26:07 +0200

    Time: 464 ms (44.02%) | Memory: 16.6 MB (54.98%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -4,27 +4,31 @@ class Solution:
         quadruplets = []
         n = len(nums)
 
-        for i in range(n - 3):
-            if i > 0 and nums[i] == nums[i - 1]:
+        for i in range(n-3):
+            if i > 0 and nums[i-1] == nums[i]:
                 continue
-            for j in range(i + 1, n - 2):
-                if j > i + 1 and nums[j] == nums[j - 1]:
+            for j in range(i+1, n-2):
+                if j > i+1 and nums[j-1] == nums[j]:
                     continue
-                left = j + 1
-                right = n - 1
+
+                left, right = j+1, n-1
+
                 while left < right:
-                    total = nums[i] + nums[j] + nums[left] + nums[right]
-                    if total == target:
+                    summ = nums[i] + nums[j] + nums[left] + nums[right]
+
+                    if summ == target:
                         quadruplets.append([nums[i], nums[j], nums[left], nums[right]])
-                        while left < right and nums[left] == nums[left + 1]:
+                        while left < right and nums[left] == nums[left+1]:
                             left += 1
-                        while left < right and nums[right] == nums[right - 1]:
-                            right -= 1
-                        left += 1
-                        right -= 1
-                    elif total < target:
-                        left += 1
+                        while left < right and nums[right] == nums[right-1]:
+                            right -= 1 
+                        
+                        left += 1 
+                        right -= 1 
+
+                    elif summ < target:
+                        left += 1 
                     else:
-                        right -= 1
+                        right -= 1 
 
         return quadruplets
