c89bd771e84c8c3335aca84f07714fc58c634971
Mohamed Abdelhalim <95293830+1abdelhalim@users.noreply.github.com>
2024-03-17 17:00:42 +0200

    Time: 51 ms (21.76%) | Memory: 17.1 MB (36.06%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,6 +1,5 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        
         n, m = len(matrix), len(matrix[0])
         low = 0 
         high = n * m - 1
