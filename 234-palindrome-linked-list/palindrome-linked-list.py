800e968b181c24d04fa124eb42be81e509e3159b
Mohamed Abdelhalim <95293830+1abdelhalim@users.noreply.github.com>
2024-03-22 22:02:37 +0200

    Time: 261 ms (97.47%) | Memory: 31.9 MB (85.63%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -1,20 +1,26 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        stack = []
-        curr = head
-        while curr:
-            stack.append(curr.val)
-            curr = curr.next
+    def isPalindrome(self, head: ListNode) -> bool:
+        def reverseList(node):
+            prev = None
+            while node:
+                next_node = node.next
+                node.next = prev
+                prev = node
+                node = next_node
+            return prev
 
-        curr = head
-        while curr:
-            if curr.val != stack.pop():
+ 
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        second_half_reversed = reverseList(slow)
+
+        while second_half_reversed:
+            if head.val != second_half_reversed.val:
                 return False
-            curr = curr.next
+            head = head.next
+            second_half_reversed = second_half_reversed.next
 
         return True
