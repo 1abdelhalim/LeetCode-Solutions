fd4c8a3de700e6686e7275c3e32ae6eff4b6c405
Mohamed Abdelhalim <95293830+1abdelhalim@users.noreply.github.com>
2024-05-21 22:39:55 +0300

    Time: 45 ms (14.02%) | Memory: 16.6 MB (10.55%) - LeetSync

--- 3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.py
+++ 3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.py
@@ -1,15 +1,9 @@
 class Solution:
     def findPermutationDifference(self, s: str, t: str) -> int:
-        s_indeces = {}
-        t_indeces = {}
+        ans = 0
         n = len(s)
-
+        
         for i in range(n):
-            s_indeces[s[i]] = i 
-            t_indeces[t[i]] = i 
-            
-        ans = 0 
-        for char in s:
-            ans += abs(s_indeces[char] - t_indeces[char])
+            ans += abs(i - t.index(s[i]))
 
-        return ans      
+        return ans
