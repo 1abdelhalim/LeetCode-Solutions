ed76e2010b99456672d91b97563fdbbd1eed6dca
Mohamed Abdelhalim <95293830+1abdelhalim@users.noreply.github.com>
2024-08-02 14:25:54 +0300

    Time: 35 ms (71.58%) | Memory: 16.4 MB (83.28%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+
+        stack = [(root.left, root.right)]
+        
+        while stack:
+            left_node, right_node = stack.pop()
+            
+            if not left_node and not right_node:
+                continue
+            
+            if not left_node or not right_node or left_node.val != right_node.val:
+                return False
+            
+            stack.append((left_node.left, right_node.right))
+            stack.append((left_node.right, right_node.left))
+        
+        return True
