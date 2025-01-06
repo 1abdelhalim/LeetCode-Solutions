8fae94043a8fb179e147b01468ae4a9e588ddee9
Mohamed Abdelhalim <95293830+1abdelhalim@users.noreply.github.com>
2025-01-06 16:59:13 +0200

    1895-MINIMUM-NUMBER-OF-OPERATIONS-TO-MOVE-ALL-BALLS-TO-EACH-BOX Stats: Time: 3 ms (99.54%), Space: 17.9 MB (41.05%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,12 +1,28 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
         n = len(boxes)
-        min_ops = [0] * n 
+        min_ops = [0] * n
 
+        # left to right 
+        balls = 0 
+        ops = 0 
 
         for i in range(n):
-            for j in range(n):
-                if boxes[j] == "1":
-                    min_ops[i] += abs(i-j)
+            min_ops[i]+= ops 
+            if boxes[i] == "1":
+                balls+=1 
+
+            ops += balls 
+
+        # right to left 
+        balls = 0 
+        ops = 0 
+        for i in range(n-1, -1, -1):
+            min_ops[i] += ops
+            if boxes[i] =="1":
+                balls += 1 
+
+            ops += balls 
+
 
         return min_ops
