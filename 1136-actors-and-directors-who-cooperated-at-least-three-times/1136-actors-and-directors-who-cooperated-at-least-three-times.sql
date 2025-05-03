28b033f4e53a69ab8288635d5e411e8a2d4f488a
Mohamed Abdelhalim <95293830+1abdelhalim@users.noreply.github.com>
2025-05-03 13:46:17 +0300

    Time: 384 ms (61.01%), Space: 0B (100%) - LeetHub

--- 1136-actors-and-directors-who-cooperated-at-least-three-times/1136-actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1136-actors-and-directors-who-cooperated-at-least-three-times/1136-actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,9 +1,4 @@
-# Write your MySQL query statement below
-Select t1.actor_id, t1.director_id 
-From ActorDirector t1 
-Join ActorDirector t2 
-On t1.actor_id = t2.actor_id
-And t1.director_id = t2.director_id 
-And t1.timestamp != t2.timestamp
-Group by 1, 2 
-Having Count(*) > 2  
+SELECT actor_id, director_id
+FROM ActorDirector
+GROUP BY actor_id, director_id
+HAVING COUNT(*) >= 3;
